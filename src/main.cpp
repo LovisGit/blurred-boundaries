ad7cb4cd09545624bb73e8c5c877084f2baea2c4
nedulau <111195742+nedulau@users.noreply.github.com>
2024-12-09 21:53:48 +0100

    neuer Pfad mit Makefile

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@ KONSTANTEN
 */
 
 // Fenstergrößen
-int             WINDOW_HEIGHT = 1920;
-int             WINDOW_WIDTH = 1080;
+int             WINDOW_HEIGHT = 1000;
+int             WINDOW_WIDTH = 1000;
 
 // Pfad zum Hintergrundbild
-std::string     SURFACEPATH = "assets/background.jpeg"; 
+std::string     SURFACEPATH = "../assets/background.jpeg"; 
 
 
 /*
@@ -38,11 +38,11 @@ int main(int argc, char* argv[]) {
     // Verschiedene weitere Initialisierungen 
     Uint32          render_flags    = SDL_RENDERER_ACCELERATED;
     SDL_Renderer*   rend            = SDL_CreateRenderer(window, -1, render_flags);
-    SDL_Surface*    surface;
+    SDL_Surface*    surface         = IMG_Load(SURFACEPATH.c_str());
     SDL_Texture*    tex             = SDL_CreateTextureFromSurface(rend, surface);
     SDL_Rect        dest;
 
-    surface = IMG_Load(SURFACEPATH);
+    
     SDL_FreeSurface(surface);
     SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
 
@@ -50,8 +50,58 @@ int main(int argc, char* argv[]) {
 
     // Game Loop 
     while (running) {
-        
+        SDL_Event event;
+
+		// Events management
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+
+			case SDL_QUIT:
+				// handling of close button
+				running = 0;
+				break;
+
+			case SDL_KEYDOWN:
+				// keyboard API for key pressed
+				switch (event.key.keysym.scancode) {
+				case SDL_SCANCODE_W:
+				case SDL_SCANCODE_UP:
+					//y = dest.y - speed / 30;
+					//y = dest.y - 1;
+					break;
+				case SDL_SCANCODE_A:
+				case SDL_SCANCODE_LEFT:
+					//x = dest.x - speed / 30;
+
+					break;
+				case SDL_SCANCODE_S:
+				case SDL_SCANCODE_DOWN:
+					//y = dest.y + speed / 30;
+
+					break;
+				case SDL_SCANCODE_D:
+				case SDL_SCANCODE_RIGHT:
+					//x = dest.x + speed / 30;
+
+					break;
+				default:
+					break;
+				}
+			}
+        }
     }
+
+	// destroy texture
+	SDL_DestroyTexture(tex);
+
+	// destroy renderer
+	SDL_DestroyRenderer(rend);
+
+	// destroy window
+	SDL_DestroyWindow(window);
+	
+	// close SDL
+	SDL_Quit();
 
     return 0;
 }
